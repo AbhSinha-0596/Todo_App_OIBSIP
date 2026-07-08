68f76686db94893a627a9d572449bcce0161d7ca
AbhijitSinha <abhijitcoding0508@gmail.com>
2026-07-08 16:15:26 +0530

    Add files via upload

---
+++ todo_project/src/navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const navbar = () => {
+  
+  return (
+    
+    <nav className="bg-sky-800 min-w-100 flex justify-between py-2">
+      <div>
+        <span className="text-white font-bold mx-4  ">To-Do</span>
+        
+        </div>
+      
+    </nav>
+  )
+
+}
+
+export default navbar
